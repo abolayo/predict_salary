9b536729b40df79e9da3593a13c10e324e469df7
abolayo <medinatapampa@yahoo.com>
2024-03-27 15:48:15 -0300

    modified

--- predict_page.py
+++ predict_page.py
@@ -5,56 +5,57 @@ import numpy as np
 
 def load_model():
     with open('saved_steps.pkl', 'rb') as file:
-        data = pickle.load(file)
-    return data
+        info = pickle.load(file)
+    return info
 
-data = load_model()
 
-regressor = data["model"]
-le_country = data["le_country"]
-le_education = data["le_education"]
+class Predict:
+    data = load_model()
+    regressor = data["model"]
+    le_country = data["le_country"]
+    le_education = data["le_education"]
 
-def show_predict_page():
-    st.title("Software Developer Salary Prediction")
+    def show_predict_page(self, le_country=None, le_education=None, regressor=None):
+        st.title("Software Developer Salary Prediction")
 
-    st.write("""### We need some information to predict the salary""")
+        st.write("""### We need some information to predict the salary""")
 
-    countries = (
-        "United States of America",
-        "India",
-        "United Kingdom of Great Britain and Northern Ireland",
-        "Germany",
-        "Canada",
-        "Brazil",
-        "France",
-        "Spain",
-        "Australia",
-        "Netherlands",
-        "Poland",
-        "Italy",
-        "Russian Federation",
-        "Sweden",
-        "Switzerland"           
-    )
+        countries = (
+            "United States of America",
+            "India",
+            "United Kingdom of Great Britain and Northern Ireland",
+            "Germany",
+            "Canada",
+            "Brazil",
+            "France",
+            "Spain",
+            "Australia",
+            "Netherlands",
+            "Poland",
+            "Italy",
+            "Russian Federation",
+            "Sweden",
+            "Switzerland"
+        )
 
-    education = (
-        "Less than a Bachelors",
-        "Bachelor’s degree",
-        "Master’s degree",
-        "Post grad",
-    )
+        education = (
+            "Less than a Bachelors",
+            "Bachelor’s degree",
+            "Master’s degree",
+            "Post grad",
+        )
 
-    country = st.selectbox("Country", countries)
-    education = st.selectbox("Education Level", education)
+        country = st.selectbox("Country", countries)
+        education = st.selectbox("Education Level", education)
 
-    expericence = st.slider("Years of Experience", 0, 50, 3)
+        expericence = st.slider("Years of Experience", 0, 50, 3)
 
-    ok = st.button("Calculate Salary")
-    if ok:
-        X = np.array([[country, education, expericence ]])
-        X[:, 0] = le_country.transform(X[:,0])
-        X[:, 1] = le_education.transform(X[:,1])
-        X = X.astype(float)
+        ok = st.button("Calculate Salary")
+        if ok:
+            x = np.array([[country, education, expericence]])
+            x[:, 0] = le_country.transform(x[:, 0])
+            x[:, 1] = le_education.transform(x[:, 1])
+            x = x.astype(float)
 
-        salary = regressor.predict(X)
-        st.subheader(f"The estimated salary is ${salary[0]:.2f}")
+            salary = regressor.predict(x)
+            st.subheader(f"The estimated salary is ${salary[0]:.2f}")
