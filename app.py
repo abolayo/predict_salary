890b55e6d7bd6778a0edc16da4d5d7f1a369ef83
abolayo <medinatapampa@yahoo.com>
2024-04-15 19:32:40 -0300

    refactor

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
 import streamlit as st
-from predict_page import show_predict_page
-from explore_page import show_explore_page
 
+from pages.explore_page import show_explore_page
 
-page = st.sidebar.selectbox("Explore Or Predict", ("Predict", "Explore"))
+from pages.predict_page import show_predict_page
+
+page = st.sidebar.selectbox("Explore Or Predict", ["Predict", "Explore"])
 
 if page == "Predict":
     show_predict_page()
